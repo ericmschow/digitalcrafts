496fa1edd2814b2ab370f027febcac62ef923d72
ericmschow <eric.m.schow@gmail.com>
2017-04-18 10:49:06 -0500

    completed ex7

---
+++ python-exercises-1/ex7.py
@@ -0,0 +1,19 @@
+bill = float(input("What is the total bill amount? Please omit the dollar sign. "))
+rawServiceLevel = input("How did they do? Good, fair, or bad? ")
+
+
+serviceLevel = rawServiceLevel.lower()
+
+tipAmount = {
+    "good" : .2,
+    "fair" : .15,
+    "bad" : .1
+}
+
+def tipCalc():
+    tip = bill * tipAmount[serviceLevel]
+    total = bill * tip
+    print("The tip amount is ${}.".format(tip))
+    print("And the total bill is ${}.".format(bill))
+
+tipCalc()
