7c41ed7d191d23fd25c09b6604c38e2799029413
ericmschow <eric.m.schow@gmail.com>
2017-04-19 13:17:48 -0500

    clarified print statement & removed debug print

--- python-exercises-2/triangle.py
+++ python-exercises-2/triangle.py
@@ -4,12 +4,16 @@ def promptHeight():
 def looper(height):
     for i in range(1, height+1):
         # print(i) # debug print line
-        print(" " * (height - i) + "*" * (2 * i - 1) + " " * (height - i))
+        print(
+        " " * (height - i),
+        "*" * (2 * i - 1),
+        " " * (height - i)
+        )
 
 
 #checks whether user inputs are positive
 def checker(lBound):
-    print(lBound) # debug print line
+    # print(lBound) # debug print line
     if lBound > 0:
         looper(lBound)
     else:
