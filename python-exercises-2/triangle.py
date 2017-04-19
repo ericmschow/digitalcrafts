04d234f2499cf058576c688070529ece1bbd39f2
ericmschow <eric.m.schow@gmail.com>
2017-04-19 13:09:10 -0500

    completed triangle, both versions

---
+++ python-exercises-2/triangle.py
@@ -0,0 +1,18 @@
+def promptHeight():
+    return int(input("What is the height? "))
+
+def looper(height):
+    for i in range(1, height+1):
+        # print(i) # debug print line
+        print(" " * (height - i) + "*" * (2 * i - 1) + " " * (height - i))
+
+
+#checks whether user inputs are positive
+def checker(lBound):
+    print(lBound) # debug print line
+    if lBound > 0:
+        looper(lBound)
+    else:
+        print("ERROR: size cannot be <= 0.")
+
+checker(promptHeight())
