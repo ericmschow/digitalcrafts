53cfc1a638bbe73997307c65973e653d0207fbe8
ericmschow <eric.m.schow@gmail.com>
2017-04-19 11:41:33 -0500

    Completed n_to_m

---
+++ python-exercises-2/n_to_m.py
@@ -0,0 +1,16 @@
+def promptLo():
+    return int(input("Where would you like to start? "))
+def promptHi():
+    return int(input("Where would you like to stop? "))
+
+def looper(lBound, uBound):
+    for i in range(lBound, uBound+1):
+        print(i)
+def checker(lBound, uBound):
+    if lBound < uBound:
+        looper(promptLo(), promptHi())
+    else:
+        print("ERROR: start not less than end.")
+
+looper(promptLo(), promptHi())
+#checker(promptLo(), promptHi())
