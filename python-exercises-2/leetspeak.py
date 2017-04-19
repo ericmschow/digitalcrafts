b04339de188c3a3ba947589f69a7a420de8e456b
ericmschow <eric.m.schow@gmail.com>
2017-04-19 10:49:26 -0500

    completed leetspeak

---
+++ python-exercises-2/leetspeak.py
@@ -0,0 +1,22 @@
+leet = {
+    'A' : '4',
+    'E' : '3',
+    'G' : '6',
+    'I' : '1',
+    'O' : '0',
+    'S' : '5',
+    'T' : '7'
+}
+#wordList = []
+def translator():
+    string = input("Please give me a string: ")
+    for char in string:
+        try:
+            leet[char.upper()]
+            wordList.append(leet[char.upper()])
+        except KeyError:
+            wordList.append(char)
+    wordStr = ''.join(wordList)
+    print(wordStr)
+
+translator()
