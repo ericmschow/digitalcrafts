489ee1accf2d326ec0e1f1b646d8c05e1b8cf6d6
ericmschow <eric.m.schow@gmail.com>
2017-04-24 10:10:25 -0500

    completed dictionary exercise 2, using nested dicts

---
+++ python-exercises-4/dictexercise2.py
@@ -0,0 +1,22 @@
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+print(ramit['email'])
+print(ramit['interests'][0])
+print("Jasmine's email is ",ramit['friends'][0]['email'])
+print("Jan's interest is ", ramit['friends'][1]['interests'][1])
