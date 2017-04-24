36e411e584292c76ae4d5d5e78480369659d0c2d
ericmschow <eric.m.schow@gmail.com>
2017-04-24 14:14:28 -0500

    updated reader to output histogram

--- python-exercises-4/reader.py
+++ python-exercises-4/reader.py
@@ -1,8 +1,28 @@
+histogram = {}
+
+def word_histogram(text):
+    words = text.split(' ')
+    for word in words:
+        count = words.count(word)
+        if word not in histogram and word != '': # to prevent logging spaces
+            histogram.update({word:count})
+
+def letter_histogram(word):
+    for char in word:
+        count = word.count(char)
+        if char not in histogram:
+            histogram.update({char:count})
+
 def main():
-    filename = input("Please enter a filename. >")
+    filename = input("Please enter a filename. > ")
     with open(filename, 'r') as fh:
         txt = fh.read()
-    print( txt)
+    letter_histogram(txt)
+    print(histogram)
+    histogram.clear()
+    word_histogram(txt)
+    print(histogram)
+
 
 if __name__ == '__main__':
     main()
