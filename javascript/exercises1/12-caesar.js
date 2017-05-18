65e759c7e60f012d4bc129b6cdd9f7d282db118c
ericmschow <eric.m.schow@gmail.com>
2017-05-18 13:01:30 -0500

    completed ex 12

---
+++ javascript/exercises1/12-caesar.js
@@ -0,0 +1,22 @@
+function caesar(string, offset) {
+  let cipher = []
+  for (i = 0; i < string.length; i++ ) {
+    char = string[i];
+    let charcode = char.charCodeAt(0)
+    if (charcode >= 65 && charcode < 91 || charcode >= 97 && charcode < 123) {
+      let newcharcode = (charcode + offset)
+      if (newcharcode > 122 || (newcharcode >= 91 && newcharcode < 97) ) {
+        newcharcode = newcharcode - 26;
+      }
+      var newchar = String.fromCharCode(newcharcode)
+    }
+    else {
+      var newchar = char
+    }
+    cipher.push(newchar)
+  }
+
+  return cipher.join('')
+}
+
+console.log(caesar('abcdefghijklmnopqrstuvwxyz./?,<>;{}][\';\']\''.toUpperCase(), 1))
