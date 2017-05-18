7975d2512e5bba48e53b5b5c35678989c9b604c8
ericmschow <eric.m.schow@gmail.com>
2017-05-18 10:57:22 -0500

    completed ex3

---
+++ javascript/exercises1/3-madlib.js
@@ -0,0 +1,6 @@
+function madlib(name, subject) {
+  console.log(`${name}'s favorite subject is ${subject}.`)
+  return `${name}'s favorite subject is ${subject}.`
+
+};
+madlib('Erik', 'javascript');
