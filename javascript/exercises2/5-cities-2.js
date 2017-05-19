91ac294cc5d0fd1bd32f61a318ca499aee7ab23c
ericmschow <eric.m.schow@gmail.com>
2017-05-19 13:40:05 -0500

    completed ex5

---
+++ javascript/exercises2/5-cities-2.js
@@ -0,0 +1,14 @@
+function justCities(list) {
+  return (list.map(function(n) {
+    return n.name}
+  ))
+}
+
+var cities = [
+  { name: 'Los Angeles', temperature: 60.0},
+  { name: 'Atlanta', temperature: 52.0 },
+  { name: 'Detroit', temperature: 48.0 },
+  { name: 'New York', temperature: 80.0 }
+];
+
+console.log(justCities(cities))
