f3f9925300a2f1dd3b0485e8c7fd271549252b46
ericmschow <eric.m.schow@gmail.com>
2017-05-19 13:49:42 -0500

    completed ex 7

---
+++ javascript/exercises2/7-sortarray-1.js
@@ -0,0 +1,27 @@
+function alphaSort(list) {
+  return list.sort()
+
+
+};
+
+
+
+var people = [
+  'Dom',
+  'Lyn',
+  'Kirk',
+  'Autumn',
+  'Trista',
+  'Jesslyn',
+  'Kevin',
+  'John',
+  'Eli',
+  'Juan',
+  'Robert',
+  'Keyur',
+  'Jason',
+  'Che',
+  'Ben'
+];
+
+console.log(alphaSort(people));
