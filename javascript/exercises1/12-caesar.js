4d626ddaefc4e5feb82cd7cca9717e0ebee5d966
ericmschow <eric.m.schow@gmail.com>
2017-05-23 09:45:41 -0500

    fixed git issue

--- javascript/exercises1/12-caesar.js
+++ javascript/exercises1/12-caesar.js
@@ -5,7 +5,7 @@ function caesar(string, offset) {
     let charcode = char.charCodeAt(0)
     if (charcode >= 65 && charcode < 91 || charcode >= 97 && charcode < 123) {
       let newcharcode = (charcode + offset)
-      if (!(newcharcode >= 65 && newcharcode < 91 || newcharcode >= 97 && newcharcode < 123) ) {
+      if (newcharcode > 122 || (newcharcode >= 91 && newcharcode < 97) ) {
         newcharcode = newcharcode - 26;
       }
       var newchar = String.fromCharCode(newcharcode)
@@ -19,4 +19,4 @@ function caesar(string, offset) {
   return cipher.join('')
 }
 
-console.log(caesar('abcdefghijklmnopqrstuvwxyz'.toUpperCase(), 13))
+console.log(caesar('abcdefghijklmnopqrstuvwxyz./?,<>;{}][\';\']\''.toUpperCase(), 1))
--- python-exercises-4/100_summer.py
+++
@@ -1,8 +0,0 @@
-# program should output all possibilities to put + or - between the numbers
-# in order 1 2 3 4 5 6 7 8 9 such that the result is 100
-# e.g. 1 + 2 + 3 - 4 + 5 + 6 + 78 + 9 = 100
-
-
-
-if __name__ == '__main__':
-    main()
