ea220a871826687cfbb8c760bce092f3da7661b5
ericmschow <eric.m.schow@gmail.com>
2017-06-06 11:35:01 -0500

    completed ex6

--- express/exercises-1/app.js
+++ express/exercises-1/app.js
@@ -10,15 +10,15 @@ app.get('/', function(request, response) {
 });
 
 app.get('/cats', function(request, response) {
-  response.send('MEOWWW')
+  response.render('cats.hbs')
 });
 
 app.get('/dogs', function(request, response) {
-  response.send('WOOFFF')
+  response.render('dogs.hbs')
 });
 
 app.get('/cats_and_dogs', function(request, response) {
-  response.send('Living together')
+  response.render('cats_and_dogs.hbs')
 });
 
 app.get('/greet/:name', function(request, response) {
@@ -31,6 +31,19 @@ app.get('/greet/:name', function(request, response) {
   }
   response.render('greet.hbs', context);
 })
+
+app.get('/fav_animals', function(request, response){
+  var animals = [
+    { name: 'cats', favorite: true },
+    { name: 'dogs', favorite: true },
+    { name: 'tree frogs', favorite: true },
+    { name: 'earth worms', favorite: false },
+    { name: 'guinea pigs', favorite: true },
+  ];
+  context = {animals: animals}
+  response.render('fav_animals.hbs', context)
+})
+
 
 app.get('/age/:year', function(request, response) {
   let year = request.params.year;
