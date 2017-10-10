a73fcb70de0538d2950fbf9b8db218cb3d196dad
ericmschow <eric.m.schow@gmail.com>
2017-10-10 18:26:37 -0500

    summation version of interview1

--- CS-Elective/interview1.py
+++ CS-Elective/interview1.py
@@ -9,12 +9,18 @@ class example():
         """Setup
         """
         pass
+    def mathfn(self, num, max):
+        max -= 1
+        p = int(max / num)
+        sum = num * (p * (p + 1)) / 2
+        return int(sum)
     def main(self):
         max = 50000000
         sum = 0
-        for i in range(3, max):
-            if i % 3 == 0 or i % 5 == 0:
-                sum += i
+        # for i in range(3, max):
+        #     if i % 3 == 0 or i % 5 == 0:
+        #         sum += i
+        sum += (self.mathfn(3, max) + self.mathfn(5, max) - self.mathfn(15, max))
         return sum
 if __name__ == "__main__":
     main = example()
